32db47031094734ae31a22e7e9bfa51476d6758d
Matias Falcone <matias.falcone@gmail.com>
2020-05-20 12:45:07 +1000

    Updates

---
+++ precache-manifest.2d542a9f1f0bde4aebe8e3b5fdae7da7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5776f854c208ae242ac5a8028349b49",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "ba15dbd2d87b51e4775b",
+    "url": "/website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/website/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba15dbd2d87b51e4775b",
+    "url": "/website/static/js/main.5283a39e.chunk.js"
+  },
+  {
+    "revision": "e7c5d6fbd36102063d36",
+    "url": "/website/static/js/runtime-main.b25d9334.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
